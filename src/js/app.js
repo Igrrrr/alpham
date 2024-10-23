1efc6e94857c01abb3bb47ecd1796fb605617092
Igor Konovodov <konovodov@list.ru>
2024-10-23 22:54:00 +0300

    fix: menu-burger

--- src/js/app.js
+++ src/js/app.js
@@ -1,72 +1,18 @@
-import * as bootstrap from "./modules/bootstrap.bundle.min.js";
-import reviews from "./modules/reviews.js";
+// const headerTop = document.querySelector(".header__top");
+// const searchSection = document.querySelector(".search");
 
-const splideList = document.querySelector(".splide__list");
-const reviewsQuantatyIndex = document.querySelector(".reviews__quantity-index");
-const reviewsQuantatyTotal = document.querySelector(".reviews__quantity-total");
+// const burger = document.getElementById("burger-toggle");
 
-const slider = new Splide("#slider", {
-  perPage: 4,
-  gap: "1rem",
-  flickMaxPages: 1,
-  perPage: 3,
-  arrows: false,
-  breakpoints: {
-    1199: {
-      perPage: 2,
-    },
-    767: {
-      perPage: 1,
-    },
-  },
-});
+// const hideElements = (e, el) => {
+//   e.target.checked
+//     ? el.classList.add("hidden-content")
+//     : el.classList.remove("hidden-content");
+// };
 
-const setReviews = () => {
-  reviews.forEach((el) => {
-    let slide = document.createElement("li");
-    slide.className = "splide__slide";
+// const changeVisibleState = (...args) => {
+//   args.forEach((arg) => hideElements(e, arg));
+// };
 
-    let date = document.createElement("p");
-    date.className = "splide__slide-date";
-    date.innerText = el.date;
-    slide.append(date);
+// changeVisibleState(headerTop, searchSection);
 
-    let name = document.createElement("p");
-    name.className = "splide__slide-title";
-    name.innerText = `${el.name} (${el.playersQuantity})`;
-    slide.append(name);
-
-    let raiting = document.createElement("div");
-    raiting.className = "splide__slide-raiting";
-    for (let i = 0; i < el.raiting; i++) {
-      let raitingStar = document.createElement("img");
-      raitingStar.src = "./img/star.svg";
-      raiting.append(raitingStar);
-    }
-    slide.append(raiting);
-
-    let event = document.createElement("p");
-    event.className = "splide__slide-event";
-    event.innerText = el.event;
-    slide.append(event);
-
-    let feedback = document.createElement("p");
-    feedback.className = "splide__slide-feedback";
-    feedback.innerText = el.feedback;
-    slide.append(feedback);
-
-    splideList.append(slide);
-  });
-};
-
-setReviews();
-slider.mount();
-
-const setReviewsQuantity = () => {
-  reviewsQuantatyTotal.innerText = slider.length;
-  reviewsQuantatyIndex.innerText = `${slider.index + 1} / `;
-};
-
-setReviewsQuantity();
-
-slider.on("moved", setReviewsQuantity);
+// burger.addEventListener("change", changeVisibleState);
